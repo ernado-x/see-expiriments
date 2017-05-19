f16dc8b75ac8e119090e312a08da850ecc360c51
Andrew Gubskiy <andrew.gubskiy@gmail.com>
2017-05-19 15:27:25 +0300

    Update getNowDate

--- src/WebClient/js/common.js
+++ src/WebClient/js/common.js
@@ -13,6 +13,7 @@ function guid() {
 }
 
 function getNowDate() {
+    var today = new Date();
     var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
     var dateTime = date + ' ' + time;
